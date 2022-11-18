168b9bd970ca5d99fb56b72d0af652c0db951887
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-11-18 10:03:25 +0100

    Add files via upload

---
+++ delivery_inherit/model/delivery_inherit_model.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+
+class StockHerit(models.Model):
+    _inherit = 'stock.picking'
+
+    state = fields.Selection(selection_add=[('delivery', 'Bon de livraison'), ('done',)])
+    delivery_sign = fields.Binary('Signature')
+
+
+    def print_delivery(self):
+        if self.state != "done":
+            self.write({'state': "delivery"})
+        return self.env.ref('stock.action_report_delivery').report_action(self)
+
+    def print_reprise(self):
+        return self.env.ref('stock.action_report_delivery').report_action(self)
